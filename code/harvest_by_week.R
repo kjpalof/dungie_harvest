5361f4d0207f196597c79867ee5399b9aec47225
Katie Palof <katie.palof@alaska.gov>
2017-06-29 08:22:59 -0800

    initial code file with packages to load thus far

---
+++ code/harvest_by_week.R
@@ -0,0 +1,9 @@
+# K.Palof 6-29-17
+# Purpose of this script: to examine Dungeness harvest % by week in the fishery the last 5 years to look at current year (2017) closing early
+#
+
+### Load packages ----
+library(tidyverse)
+library(reshape2)
+
+#### Load data ----
