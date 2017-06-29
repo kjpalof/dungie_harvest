8ac86ca6d20c9d38bb8ed1ab0549d4252dfe8bd3
Katie Palof <katie.palof@alaska.gov>
2017-06-29 08:49:11 -0800

    working on getting percent by week, right now it's by day

--- code/harvest_by_week.R
+++ code/harvest_by_week.R
@@ -20,12 +20,26 @@ dunge %>% select(year = YEAR, SEASON, TICKET = TICKET_NO, cdate = CATCH_DATE, ST
 ### stat week -----
 dunge1 %>% 
   group_by(SEASON, STAT_WEEK) %>% 
-  summarise (sum(numbers), sum(pounds)) ->sum_stat_week
+  summarise (numbers = sum(numbers), pounds = sum(pounds)) ->sum_stat_week
 
-
+#### total season ----
 dunge1 %>% 
   group_by(SEASON) %>% 
-  summarise(sum(numbers), sum(pounds)) -> total_season
+  summarise(totalN = sum(numbers), totalP =sum(pounds)) -> total_season
 
 
 #### by day ----
+dunge1 %>% 
+  group_by(SEASON, selld) %>% 
+  summarise (numbers = sum(numbers), pounds = sum(pounds)) -> sum_by_day
+
+### not sure -----
+sum_by_day %>% 
+  left_join(total_season) -> dunge2
+
+dunge2 %>% 
+  mutate(per_total_N = numbers/ totalN, per_total_P = pounds/totalP) ->dunge3
+
+
+dunge3
+
