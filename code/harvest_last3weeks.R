ddaeb64fd3932c57a41f8da08c09f8cb27219c6c
Katie Palof <katie.palof@alaska.gov>
2018-04-10 13:19:13 -0800

    code to determine harvest percentage the last three weeks of fishery - initial

---
+++ code/harvest_last3weeks.R
@@ -0,0 +1,34 @@
+# Southeast Alaska Dungeness harvest - updated through 2017
+# 
+# K.Palof   ADF&G
+# katie.palof@alaska.gov
+# 2018-4-10
+
+# This code will establish the percentage of harvest by week in the Dungeness crab fishery.  
+# Specifically looking at determining what percentage of harvest is taken in the last 3 weeks of the fishery
+#     since in 2017 the fishery was closed 3 weeks early due to conservation concerns.
+
+# All catch values are in pounds unless otherwise noted.
+
+
+# load ----
+library(tidyverse)
+library(reshape2)
+library(xlsx)
+library(extrafont)
+options(scipen=9999) # remove scientific notation
+
+loadfonts(device="win")
+windowsFonts(Times=windowsFont("TT Times New Roman"))
+theme_set(theme_bw(base_size=12,base_family='Times New Roman')+ 
+            theme(panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank()))
+
+
+#### Load data ----
+dunge <- read.csv("./data/Dungeness_12_16.csv") # tanner crab survey data
+glimpse(dunge)
+
+### reduce data ----
+dunge %>% select(year = YEAR, SEASON, TICKET = TICKET_NO, cdate = CATCH_DATE, STAT_WEEK, selld = SELL_DATE, 
+                 numbers = NUMBERS, pounds = POUNDS) -> dunge1
