18f2477d6cad25d6de6e7b452c78969ac94af024
Katie Palof <katie.palof@alaska.gov>
2018-04-11 12:34:29 -0800

    week as variable (column) for standard summer and fall season

--- code/harvest_last3weeks.R
+++ code/harvest_last3weeks.R
@@ -39,15 +39,28 @@ dunge1 %>%
   mutate(c_date = as.Date(cdate, format = "%m/%d/%Y"), 
          cdate_mo = month(c_date), cdate_day = day(c_date)) -> test
          
-# need to create a key for season weeks
+# Summer season: June 15th - Aug 15th, fall season Oct. 1 to Nov.30th
+# need to create a key for season weeks / so far only summer / add fall
 week <- c(rep(1, 7), rep(2,7), rep(3,7), rep(4,7), rep(5,7), rep(6,7), rep(7,7), rep(8,7), rep(9,6)) # 9 weeks in summer season
 month <- c(rep(6, 16), rep(7, 31), rep(8,15))
 day <- c(seq(from = 15, to =30), seq(1:31), seq(1:15))
-
+#summer
 season_week <- data.frame(week, month, day)
+
+# fall
+week <- c(rep(10, 7), rep(12,7), rep(13,7), rep(14,7), rep(15,7), rep(16,7), rep(17,7), rep(18,7), rep(19,5)) # 9 weeks in summer season
+month <- c(rep(10, 31), rep(11, 30))
+day <- c(seq(from = 1, to =31), seq(1:30))
+
+season_week_fall <- data.frame(week, month, day)
+
+# summer and fall - until Nov. 30th together
 season_week %>% 
-  mutate(cdate_mo = month, cdate_day = day) -> season_week
+  bind_rows(season_week_fall) -> season_week_all
+
+season_week_all %>% 
+  mutate(cdate_mo = month, cdate_day = day) -> season_week_all
   
 # bind season week to test to get a variable for week of the season in each year
 test %>% 
-  left_join(season_week) ->test2
+  left_join(season_week_all) ->test2
